92fa7eda9aa83957bc289f567a84f08c3bc0b7d7
wolfGuidao <2351903911@qq.com>
2020-03-04 10:47:56 +0800

    提交多种通用的Makefile及Header.hpp

---
+++ tool/MyMakefile/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("i am  main.c file\n");
+  return 0;
+}
---
+++ tool/MyMakefile/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+  std::cout<<"i am test.cpp file"<<std::endl;
+  return 0;
+}

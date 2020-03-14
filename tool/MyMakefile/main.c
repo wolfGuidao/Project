2de66d786a1bc5f85c51954f11966c0cd56ec68d
wolfGuidao <2351903911@qq.com>
2020-03-14 17:46:19 +0800

    update

--- tool/MyMakefile/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-  printf("i am  main.c file\n");
-  return 0;
-}
--- tool/MyMakefile/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-  std::cout<<"i am test.cpp file"<<std::endl;
-  return 0;
-}
